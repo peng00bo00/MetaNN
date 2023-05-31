540e7f5de83107a1a8627393bde3d0e817718e71
peng00bo00 <pengbo_tongji@126.com>
2023-05-31 20:57:31 +0400

    update

---
+++ MetaNN/facilities/_.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <MetaNN/facilities/cont_metafuns/_.h>
+#include <MetaNN/facilities/null_param.h>
+#include <MetaNN/facilities/traits.h>
+#include <MetaNN/facilities/type_id.h>
+#include <MetaNN/facilities/var_type_dict.h>
