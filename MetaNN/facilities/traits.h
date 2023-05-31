e1b1ac626f15fcc23b19ec65c8c0be4b4215ac8f
peng00bo00 <pengbo_tongji@126.com>
2023-05-31 22:22:55 +0400

    update

--- MetaNN/facilities/traits.h
+++ MetaNN/facilities/traits.h
@@ -1,1 +1,43 @@
 #pragma once
+
+#include <cstddef>
+#include <iterator>
+#include <type_traits>
+
+namespace MetaNN
+{
+
+// Identity
+template <typename T>
+struct Identity_
+{
+    using type = T;
+};
+
+// Or
+template <bool cur, typename TNext>
+constexpr static bool OrValue = true;
+
+template <typename TNext>
+constexpr static bool OrValue<false, TNext> = TNext::value;
+
+// And
+template <bool cur, typename TNext>
+constexpr static bool AndValue = false;
+
+template <typename TNext>
+constexpr static bool AndValue<true, TNext> = TNext::value;
+
+// remvoe const &
+template <typename T>
+using RemConstRef = std::remove_cv_t<std::remove_reference_t<T>>;
+
+// dependency false
+template <typename... T>
+constexpr static bool DependencyFalse = false;
+
+// compile-time switch
+template <typename TBooleanCont, typename TFunCont>
+struct CompileTimeSwitch_;
+
+}
