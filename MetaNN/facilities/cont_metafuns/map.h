51cbc75a6acd3fa56acea286f727968491ed2d28
peng00bo00 <pengbo_tongji@126.com>
2023-06-03 15:53:04 +0400

    update

--- MetaNN/facilities/cont_metafuns/map.h
+++ MetaNN/facilities/cont_metafuns/map.h
@@ -5,5 +5,33 @@
 
 namespace MetaNN::Map
 {
-    
+
+// Find ===================================================================================
+namespace NSFind
+{
+template <typename TCon, typename TDefault>
+struct map_;
+
+template <template <typename...> typename TCon, typename... TItem,
+          typename TDefault>
+struct map_<TCon<TItem...>, TDefault> : TItem...
+{
+    using TItem::apply ...;
+    static TDefault apply(...);
+};
+}
+
+template <typename TCon, typename TKey, typename TDefault>
+struct Find_
+{
+    using type = decltype(NSFind::map_<TCon, TDefault>::apply((TKey*)nullptr));
+};
+
+template <typename TCon, typename TKey, typename TDefault>
+using Find = typename Find_<TCon, TKey, TDefault>::type;
+
+// HasKey =================================================================================
+template <typename TCon, typename TKey>
+constexpr static bool HasKey = !std::is_same_v<Find<TCon, TKey>, void>;
+
 }
--- MetaNN/facilities/cont_metafuns/multi_map.h
+++ MetaNN/facilities/cont_metafuns/multi_map.h
@@ -1,3 +1,8 @@
 #pragma once
 #include <MetaNN/facilities/cont_metafuns/helpers.h>
 #include <MetaNN/facilities/cont_metafuns/sequential.h>
+
+namespace MetaNN::MultiMap
+{
+
+}
