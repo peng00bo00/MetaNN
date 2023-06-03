0b6c85c46b67df060cddcbfad0b7847b343f5b1b
peng00bo00 <pengbo_tongji@126.com>
2023-06-03 22:25:59 +0400

    Update set.h

--- MetaNN/facilities/cont_metafuns/set.h
+++ MetaNN/facilities/cont_metafuns/set.h
@@ -10,12 +10,12 @@ namespace NSHasKey
 template <typename TCon>
 struct map_;
 
-// template <template <typename... > typename TCon, typename...TItem>
-// struct map_<TCon<TItem...>> : Helper::KVBinder<TItem, Helper::Int_<true>>...
-// {
-//     using Helper::KVBinder<TItem, Helper::Int_<true>>::apply ...;
-//     static Helper::Int_<false> apply(...);
-// };
+template <template <typename... > typename TCon, typename...TItem>
+struct map_<TCon<TItem...>> : Helper::KVBinder<TItem, Helper::Int_<true>>...
+{
+    using Helper::KVBinder<TItem, Helper::Int_<true>>::apply ...;
+    static Helper::Int_<false> apply(...);
+};
 }
 
 template <typename TCon, typename TKey>
@@ -26,7 +26,25 @@ struct HasKey_
 
 template <typename TCon, typename TKey>
 constexpr bool HasKey = HasKey_<TCon, TKey>::value;
+//=========================================================================================
 
+// Insert =================================================================================
+template <typename TCon, typename TKey, bool bMute = false>
+struct Insert_ : Sequential::PushBack_<TCon, TKey>
+{
+    static_assert(!HasKey<TCon, TKey>);
+};
+
+template <typename TCon, typename TKey>
+struct Insert_<TCon, TKey, true>
+{
+    using type = typename std::conditional_t<HasKey<TCon, TKey>,
+                                             Identity_<TCon>,
+                                             Sequential::PushBack_<TCon, TKey>>::type;
+};
+
+template <typename TCon, typename TKey, bool bMute = false>
+using Insert = typename Insert_<TCon, TKey, bMute>::type;
 //=========================================================================================
 
 // IsEqual ================================================================================
