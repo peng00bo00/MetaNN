d385d07a620df8319614524e9e8232fb9cd51892
peng00bo00 <pengbo_tongji@126.com>
2023-06-04 12:41:57 +0400

    Update value_sequential.h

--- MetaNN/facilities/cont_metafuns/value_sequential.h
+++ MetaNN/facilities/cont_metafuns/value_sequential.h
@@ -3,5 +3,21 @@
 
 namespace MetaNN::ValueSequential
 {
+// Contains ===============================================================================
+template <typename TValueSeq, auto val>
+struct Contains_;
 
+template <template<auto...> class TValueCont, auto val, auto... vals>
+struct Contains_<TValueCont<vals...>, val>
+{
+    constexpr static bool value = ((vals == val) || ...);
+};
+
+template <typename TValueSeq, auto val>
+constexpr static bool Contains = Contains_<TValueSeq, val>::value;
+//=========================================================================================
+
+// Order ==================================================================================
+
+//=========================================================================================
 }
