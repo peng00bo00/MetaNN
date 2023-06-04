64f60b72fa0adc027da34cc3be7c93a955da1e24
peng00bo00 <pengbo_tongji@126.com>
2023-06-04 12:34:31 +0400

    Update multi_map.h

--- MetaNN/facilities/cont_metafuns/multi_map.h
+++ MetaNN/facilities/cont_metafuns/multi_map.h
@@ -4,6 +4,48 @@
 
 namespace MetaNN::MultiMap
 {
+// Insert =================================================================================
+namespace NSInsert
+{
+template <typename TProcessed, typename TKey, typename TValue, typename... TRemain>
+struct imp_
+{
+    using Value = Helper::ValueSequence<TValue>;
+    using type  = Sequential::PushBack<TProcessed,
+                                       Helper::KVBinder<TKey, Value>>;
+};
+
+template <typename TProcessed,
+          typename TCur, typename... TRemain,
+          typename TKey, typename TValue>
+struct imp_<TProcessed, TKey, TValue, TCur, TRemain...>
+        : imp_<Sequential::PushBack<TProcessed>, TKey, TValue, TRemain...>
+{ };
+
+template <typename TProcessed,
+          typename TKey, typename TValue,
+          typename TVs, typename... TRemain>
+struct imp_<TProcessed, TKey, TValue,
+            Helper::KVBinder<TKey, TVs>, TRemain...>
+{
+    using type = Sequential::PushBack<TProcessed,
+                                      Helper::KVBinder<TKey, Sequential::PushBack<TVs, TValue>>,
+                                      TRemain...>;
+};
+}
+
+template <typename TCon, typename TKey, typename TValue>
+struct Insert_;
+
+template <template<typename...> typename TCon, typename... TItems, typename TKey, typename TValue>
+struct Insert_<TCon<TItems...>, TKey, TValue>
+        : NSInsert::imp_<TCon<>, TKey, TValue, TItems...>
+{};
+
+template <typename TCon, typename TKey, typename TValue>
+using Insert = typename Insert_<TCon, TKey, TValue>::type;
+//=========================================================================================
+
 // Find ===================================================================================
 namespace NSFind
 {
@@ -26,5 +68,4 @@ struct Find_
 
 template <typename TCon, typename TKey>
 using Find = typename Find_<TCon, TKey>::type;
-
 }
