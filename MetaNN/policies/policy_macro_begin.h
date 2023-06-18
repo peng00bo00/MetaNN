7b1396fbff68e43a9b4c180f62c89ade737762bd
peng00bo00 <pengbo_tongji@126.com>
2023-06-18 21:48:35 +0400

    update

---
+++ MetaNN/policies/policy_macro_begin.h
@@ -0,0 +1,44 @@
+#define EnumTypePolicyObj(PolicyName, Ma, Mi, Val) \
+struct PolicyName : virtual public Ma\
+{ \
+    using MinorClass = Ma::Mi##TypeCate; \
+    using Mi = Ma::Mi##TypeCate::Val; \
+}
+
+#define TypePolicyObj(PolicyName, Ma, Mi, Val) \
+struct PolicyName : virtual public Ma\
+{ \
+    using MinorClass = Ma::Mi##TypeCate; \
+    using Mi = Val; \
+}
+
+#define ValuePolicyObj(PolicyName, Ma, Mi, Val) \
+struct PolicyName : virtual public Ma \
+{ \
+    using MinorClass = Ma::Mi##ValueCate; \
+private: \
+    using type1 = decltype(Ma::Mi); \
+    using type2 = RemConstRef<type1>; \
+public: \
+    static constexpr type2 Mi = static_cast<type2>(Val); \
+}
+
+#define TypePolicyTemplate(PolicyName, Ma, Mi) \
+template <typename T> \
+struct PolicyName : virtual public Ma \
+{ \
+    using MinorClass = Ma::Mi##TypeCate; \
+    using Mi = T; \
+}
+
+#define ValuePolicyTemplate(PolicyName, Ma, Mi) \
+template <RemConstRef<decltype(Ma::Mi)> T> \
+struct PolicyName : virtual public Ma \
+{ \
+    using MinorClass = Ma::Mi##ValueCate; \
+private: \
+    using type1 = decltype(Ma::Mi); \
+    using type2 = RemConstRef<type1>; \
+public: \
+    static constexpr type2 Mi = T; \
+}
