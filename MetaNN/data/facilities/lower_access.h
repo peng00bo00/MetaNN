3c30b5e3830b4afd23d3ca7a89cefa3193912139
peng00bo00 <pengbo_tongji@126.com>
2023-07-02 14:32:34 +0400

    Update lower_access.h

---
+++ MetaNN/data/facilities/lower_access.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <MetaNN/facilities/traits.h>
+namespace MetaNN
+{
+    /// lower access
+    template<typename TData>
+    struct LowerAccessImpl;
+
+    template <typename TData>
+    auto LowerAccess(TData&& p)
+    {
+        using RawType = RemConstRef<TData>;
+        return LowerAccessImpl<RawType>(std::forward<TData>(p));
+    }
+}
