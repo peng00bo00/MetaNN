ab06836ca81f2b4755b9440cbb86d2d8e1a89a0c
peng00bo00 <pengbo_tongji@126.com>
2023-08-06 20:47:13 +0400

    Update operation_frame.h

--- MetaNN/operation/facilities/operation_frame.h
+++ MetaNN/operation/facilities/operation_frame.h
@@ -28,7 +28,7 @@ namespace MetaNN
                       "TOperands is not an available types");
     
     public:
-        using Policies = TPolicies;
+        using Policies    = TPolicies;
         using CategoryTag = OperCateCal<TOpTag, TPolicies, TOperands...>;
         using ElementType = typename OperElementType_<TOpTag, TOperands...>::type;
         using DeviceType  = typename OperDeviceType_<TOpTag, TOperands...>::type;
@@ -36,7 +36,7 @@ namespace MetaNN
         template <size_t Id>
         using OperandType = Sequential::At<OperandContainer<TOperands...>, Id>;
     
-        public:
+    public:
         explicit Operation(TOperands... p_operands)
             : Operation(OperAuxParams<TOpTag, ElementType, CategoryTag>{},
                         std::move(p_operands)...)
