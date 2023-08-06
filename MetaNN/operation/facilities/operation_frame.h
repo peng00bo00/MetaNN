bdd7012fa2be8f59fd90749162dc2bd1a95be316
peng00bo00 <pengbo_tongji@126.com>
2023-08-06 20:43:18 +0400

    Update operation_frame.h

--- MetaNN/operation/facilities/operation_frame.h
+++ MetaNN/operation/facilities/operation_frame.h
@@ -36,6 +36,79 @@ namespace MetaNN
         template <size_t Id>
         using OperandType = Sequential::At<OperandContainer<TOperands...>, Id>;
     
+        public:
+        explicit Operation(TOperands... p_operands)
+            : Operation(OperAuxParams<TOpTag, ElementType, CategoryTag>{},
+                        std::move(p_operands)...)
+        {}
+    
+        explicit Operation(OperAuxParams<TOpTag, ElementType, CategoryTag> auxParams,
+                           TOperands... p_operands)
+            : m_auxParams(std::move(auxParams))
+            , m_shapeInfo(m_auxParams, p_operands...)
+            , m_operands({std::move(p_operands)...})
+        {}
+
+        template <size_t id>
+        const auto& Operand() const
+        {
+            return std::get<id>(m_operands);
+        }
+
+        const auto& OperandTuple() const noexcept
+        {
+            return m_operands;
+        }
+
+        const auto& AuxParams() const
+        {
+            return m_auxParams;
+        }
+
+        const auto& Shape() const 
+        {
+            return m_shapeInfo.Shape();
+        }
+
+        bool operator== (const Operation& val) const
+        {
+            // Note: shape is deduced from m_auxParams and m_operands, so if they are same, then shape is same.
+            return (m_auxParams == val.m_auxParams) &&
+                   (m_operands == val.m_operands);
+        }
+
+        auto operator[](size_t index) const
+        {
+            if constexpr (IsValidOper<OpTags::Slice, Operation>)
+            {
+                using ResType = Operation<OpTags::Slice, OperandContainer<Operation>>;
+                return ResType(OperAuxParams<OpTags::Slice,
+                                             typename ResType::ElementType,
+                                             typename ResType::CategoryTag>(index), (const Operation&)*this);
+            }
+            else
+            {
+                static_assert(DependencyFalse<Operation>, "Slice is not supported.");
+            }
+        }
+
+        auto EvalRegister() const
+        {
+            if (!m_evalBuf.IsEvaluated())
+            {
+                auto evalHandle = m_evalBuf.Handle();
+                if (!EvalPlan::Inst().IsAlreadyRegisted(evalHandle.DataPtr()))
+                {
+                    using TOperSeqCont = typename OperSeq_<TOpTag>::type;
+
+                    using THead = Sequential::Head<TOperSeqCont>;
+                    using TTail = Sequential::Tail<TOperSeqCont>;
+                    THead::template EvalRegister<TTail>(m_evalBuf, *this);
+                }
+            }
+            return m_evalBuf.ConstHandle();
+        }
+
     private:
         OperAuxParams<TOpTag, ElementType, CategoryTag> m_auxParams;
         OperShapeInfo<TOpTag, CategoryTag, TPolicies> m_shapeInfo;
